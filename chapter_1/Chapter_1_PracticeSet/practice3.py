bfec83cd6bfe65841adb4ea5c31778a806b4788e
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2025-11-04 16:34:16 +0500

    First Python Code

---
+++ chapter_1/Chapter_1_PracticeSet/practice3.py
@@ -0,0 +1,7 @@
+import pyttsx3
+
+# engine = pyttsx3.init()
+# engine.say("I will speak text one.")
+# engine.runAndWait()
+
+pyttsx3.speak("I will speak text two.")
---
+++ chapter_1/module.py
@@ -0,0 +1,8 @@
+"""My first Python code 
+do you like it ? """
+import pyjokes
+ 
+joke = pyjokes.get_jokes()
+# print("Joke is: ")
+print(joke[1])
+
