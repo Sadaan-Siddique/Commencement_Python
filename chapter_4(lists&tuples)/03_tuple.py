dc1b9f987c32203fe2ab34dbb798df5c760fbe0d
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2026-01-12 12:20:57 +0500

    name changed

--- chapter_4(lists&tuples)/03_tuple.py
+++ chapter_4(lists&tuples)/03_tuple.py
@@ -7,4 +7,23 @@
 # Allows Duplicates: Because they are indexed, tuples can have items with the same value.
 # Heterogeneous: Tuples can contain items of different data types (strings, integers, floats, and even other tuples or lists).
 
-# Difference between tuple and List : Lists are mutable while tuples aren't.
+# Difference between tuple and List : Lists are mutable while tuples aren't. Tuples are immutable
+
+# Creating a tuple:
+my_tuple = ("apple", "banana", "orange")
+coordinates = (10, 20) # Can contain mixed data types
+my_list = ["apple", "banana", "orange"]
+
+print(type(coordinates))
+print(type(my_list))
+
+mix_tuple = (1,2,false,true,"hello",1.2)
+print(type(mix_tuple))
+
+a=() # Empty Tuple
+print(type(a))
+a = (1) # this initialization will make ' a ' an integer type variable whose stored value is 1
+print(type(a))
+a = (1,) # this initialization will make ' a ' a tuple whose stored value is 1
+print(type(a))
+
